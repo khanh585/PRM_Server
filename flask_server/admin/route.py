4aebb7d6ec1860fc4f6fccd1b7629512b71bf328
KhanhTD <thanlongcr12@gmail.com>
2020-07-08 00:47:49 +0700

    test api

--- flask_server/admin/route.py
+++ flask_server/admin/route.py
@@ -1,42 +1,12 @@
 from flask import render_template, request, redirect, Blueprint
 from flask_server.dao.TodoDAO import *
 from flask_server import app
+from flask import jsonify
 todo = Blueprint('todo', __name__, url_prefix='/todo')
 
 @todo.route('/', methods=['POST','GET'])
 def index():
-    if request.method == 'POST':
-        task_content = request.form['content']
-        new_task = Todo(content = task_content)
-        try:
-            dbCreate(new_task)
-            return redirect('/')
-        except Exception as e:
-            print(e)
-            return 500,"Server error" 
-    else:
-        tasks = dbRead()
-        return render_template('index.html',tasks = tasks)
+    return jsonify({"khanh":"ahihi"})
 
 
-@todo.route('/delete/<int:id>')
-def delete(id):
-    try:
-        dbDelete(id)
-        return redirect('/')
-    except Exception as e:
-        print(e)
-        return 500,"Server error" 
 
-@todo.route('/update/<int:id>',methods=['POST','GET'])
-def update(id):
-    if request.method == 'POST':
-        content = request.form['content']
-        try: 
-            dbUpdate(id,content)
-            return redirect('/')
-        except Exception as e:
-            print(e)
-            return 500,"Server error"
-    else:
-        return render_template('update.html',task = dbGet(id))
