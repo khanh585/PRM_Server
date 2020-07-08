d3ae620b27d193075c1e3c443f9f85f8965423eb
KhanhTD <thanlongcr12@gmail.com>
2020-07-08 20:12:23 +0700

    print eror

--- flask_server/admin/actor.py
+++ flask_server/admin/actor.py
@@ -16,7 +16,7 @@ def getactor():
         return jsonify(actors), 200 
     except Exception as e:
         print(e)
-        return e, 500
+        return e.__str__, 500
 
 @actor.route('/<int:id>', methods=['GET'])
 def getactorById(id):
