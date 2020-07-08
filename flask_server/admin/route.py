119e32248d38d0518de5e82695447a336e477af1
KhanhTD <thanlongcr12@gmail.com>
2020-07-08 20:54:07 +0700

    doi driver

--- flask_server/admin/route.py
+++ flask_server/admin/route.py
@@ -2,10 +2,14 @@ from flask import render_template, request, redirect, Blueprint
 from flask_server.dao.TodoDAO import *
 from flask_server import app
 from flask import jsonify
+import pyodbc
 todo = Blueprint('todo', __name__, url_prefix='/todo')
 
 @todo.route('/', methods=['POST','GET'])
 def index():
+    for item in pyodbc.drivers():
+        print(item)
+
     return jsonify({"khanh":"ahihi"})
 
 
